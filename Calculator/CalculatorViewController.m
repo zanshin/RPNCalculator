c3a7ad367c28bf4afe2fa4ac77b944fc48964640
Mark Nichols <mark@zanshin.net>
2012-04-04 14:42:05 -0500

    Add code necessary for decimal to work.
    Added BOOL and conditionals necessary to allow numbers with decimal points to be added. Only one decimal
    point per number.

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -11,6 +11,7 @@
 
 @interface CalculatorViewController ()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic) BOOL numberHasDecimalPoint;
 @property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
@@ -18,6 +19,7 @@
 
 @synthesize display = _display;
 @synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+@synthesize numberHasDecimalPoint = _numberHasDecimalPoint;
 @synthesize brain = _brain;
 
 - (CalculatorBrain *)brain
@@ -29,14 +31,31 @@
 - (IBAction)digitPressed:(UIButton *)sender 
 {
     NSString *digit = [sender currentTitle];
-    if (self.userIsInTheMiddleOfEnteringANumber) {
+    
+    
+    if (self.userIsInTheMiddleOfEnteringANumber) 
+    {
+        if ([digit isEqualToString:@"."]) 
+        {
+            if (self.numberHasDecimalPoint) 
+                return; //can only have one decimal point per number
+            else 
+                self.numberHasDecimalPoint = YES;
+        }
         self.display.text = [self.display.text stringByAppendingString:digit];
     } else {
-        self.display.text = digit;
+        if ([digit isEqualToString:@"."]) 
+        {
+            self.display.text = @"0.";
+            self.numberHasDecimalPoint = YES;
+        } else {
+            self.display.text = digit;
+        }
         self.userIsInTheMiddleOfEnteringANumber = YES;
     }
     
 }
+
 
 - (IBAction)enterPressed 
 {
