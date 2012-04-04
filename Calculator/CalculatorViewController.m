46e79eb0f4db1db86d3f4d5633fb6231e65a2c1e
Mark Nichols <mark@zanshin.net>
2012-04-04 14:58:27 -0500

    Implemented history feature.
    As operands and operations are added they are display in a history area just above the main display.
    This history shows just the latest operands and operation to conserve space.

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -13,21 +13,28 @@
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
 @property (nonatomic) BOOL numberHasDecimalPoint;
 @property (nonatomic, strong) CalculatorBrain *brain;
+@property (nonatomic) int historyToClear;
+@property (nonatomic) BOOL clearHistory;
 @end
 
 @implementation CalculatorViewController
 
 @synthesize display = _display;
+@synthesize historyDisplay = _historyDisplay;
 @synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
 @synthesize numberHasDecimalPoint = _numberHasDecimalPoint;
 @synthesize brain = _brain;
+@synthesize historyToClear = _historyToClear;
+@synthesize clearHistory = _clearHistory;
 
+// setter for CalculatorBrain instance
 - (CalculatorBrain *)brain
 {
     if (!_brain) _brain = [[CalculatorBrain alloc] init];
     return _brain;
 }
 
+// digitPressed method
 - (IBAction)digitPressed:(UIButton *)sender 
 {
     NSString *digit = [sender currentTitle];
@@ -56,30 +63,64 @@
     
 }
 
+// enterPressed method
 - (IBAction)enterPressed 
 {
     [self.brain pushOperand:[self.display.text doubleValue]];
+    [self addToHistory:self.display.text];
     self.userIsInTheMiddleOfEnteringANumber = NO;
 }
 
+// operationPressed method
 - (IBAction)operationPressed:(UIButton *)sender 
 {
     // if the user wants they can skip the enter after the final digit
     if (self.userIsInTheMiddleOfEnteringANumber) [self enterPressed];
     
+    [self addToHistory:sender.currentTitle];
+    
     double result = [self.brain performOperation:sender.currentTitle];
     NSString *resultString = [NSString stringWithFormat:@"%g", result];
     self.display.text = resultString;
+    
+    self.historyToClear = [self.historyDisplay.text length];
+    [self addToHistory:resultString];
+    self.clearHistory = YES;
 }
 
+// clearPressed method
 - (IBAction)clearPressed:(id)sender 
 {
     // clear button pressed, clear display and historyDisplay
 
     self.display.text = @"0";
+    self.historyToClear = [self.historyDisplay.text length];
+    self.clearHistory = YES;
     [self enterPressed];
     self.userIsInTheMiddleOfEnteringANumber = NO;
 
 }
 
+- (void)viewDidUnload {
+    [self setHistoryDisplay:nil];
+    [super viewDidUnload];
+}
+
+// clear historyDisplay
+- (void)clearHistoryDisplay
+{
+    if (self.clearHistory) {
+        self.historyDisplay.text = [self.historyDisplay.text substringFromIndex:self.historyToClear];
+        self.clearHistory = NO;
+    }
+}
+
+// append operands and operations to historyDisplay UILabel
+- (void)addToHistory:(NSString *)textToAdd
+{
+    if (self.historyToClear) [self clearHistoryDisplay]; 
+    
+    self.historyDisplay.text = [self.historyDisplay.text stringByAppendingString:textToAdd];
+    self.historyDisplay.text = [self.historyDisplay.text stringByAppendingString:@" "];
+}
 @end
